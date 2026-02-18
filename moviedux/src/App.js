87c6d96fd331ead0f4f823f9820b96d3d6965393
johnciuverca <johnciuverca@gmail.com>
2026-02-18 15:00:49 +0200

    Add initial project structure with manifest, movies data, and styling

---
+++ moviedux/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css'
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+
+            <div  className='container'>
+              <Header />
+              </div>
+
+      <footer className="footer">
+            <p className='footer'>Copyright &copy; 2024 Moviedux. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
